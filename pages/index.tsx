c6ec9fa3420677c0e35fcea723189aa92b626f2a
Moses <moseslee9750@Gmail.com>
2023-01-30 03:41:07 -0800

    Add threejs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
+import Box from "../components/Box";
+import { Canvas } from "@react-three/fiber";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,16 +16,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div className="container mx-auto">
+        <div className="flex mx-auto h-screen flex-col">
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[0, 0, 0]} />
+          </Canvas>
 
-          <p className="text-white text-xl font-medium text-center"> Hi, I am Moses</p>
-
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col items-center justify-center">
-         
-              <p className="text-white text-base font-normal text-center"> I am a Software Engineer</p>
-              </div>
-              </div>
+          <div className="flex flex-col absolute z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <p className="text-white text-xl font-light text-center">
+              Hi, I am Moses
+            </p>
+            <hr />
+            <p className="text-white text-base font-light text-center">
+              full-stack engineer | artist
+            </p>
+          </div>
         </div>
       </main>
     </>
